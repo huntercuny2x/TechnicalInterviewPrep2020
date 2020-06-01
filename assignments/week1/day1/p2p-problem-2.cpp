6193e959a29781d030c54c490c4879dd56f41ce0
Suchwinder Singh <windersingh80@yahoo.com>
2020-06-01 12:51:09 -0400

    Solved To Lower Case

---
+++ assignments/week1/day1/p2p-problem-2.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for(char& c : str){
+            (c>='A' && c<='Z') ? c=c+32 : NULL;
+        }
+        return str;
+    }
+};
