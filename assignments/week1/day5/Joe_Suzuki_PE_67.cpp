09f4b3583b2f5dbdb7bbd882b42c7f2160f37263
JoeSuzuki <joesuzuki516@gmail.com>
2020-06-07 06:17:11 -0400

    Add LC 1190 & PE 67

---
+++ assignments/week1/day5/Joe_Suzuki_PE_67.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sieve_largest_pf(int num){
+	int s = sqrt(num);
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	freopen("input.txt", "r", stdin);
+	vector<int> v = {stdin};
+	return 0;
+}
+
