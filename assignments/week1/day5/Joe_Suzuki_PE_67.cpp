851fbd8922d04b2694cc6872a50660f48a831875
JoeSuzuki <joesuzuki516@gmail.com>
2020-06-07 08:23:32 -0400

    Complete PE 67

--- assignments/week1/day5/Joe_Suzuki_PE_67.cpp
+++ assignments/week1/day5/Joe_Suzuki_PE_67.cpp
@@ -1,18 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sieve_largest_pf(int num){
-	int s = sqrt(num);
-	return 0;
+void setup(vector<vector<int> >& vec){
+	ifstream fin("triangle.txt");
+	vec.resize(100);
+	for(int i = 0; i < 100; ++i){
+		vec[i].resize(i + 1);
+		for(int j = 0; j <= i; ++j){
+			int in;
+			fin >> in;
+			vec[i][j] = in;
+		}
+	}
+	fin.close();
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	vector<vector<int> > vec;
+	setup(vec);
 	
-	freopen("input.txt", "r", stdin);
-	vector<int> v = {stdin};
+	for(int i = 99; i > 0; --i)
+		for(int j = 0; j < i; ++j)
+			vec[i - 1][j] += max(vec[i][j], vec[i][j + 1]);
+	cout << vec[0][0] << endl;
+
 	return 0;
 }
 
