c34fecdcec23085b878c061f02a948df300bfbde
svhnstorage <32282010+svhnstorage@users.noreply.github.com>
2020-06-09 15:33:58 -0400

    Create Create Saurav_Hossain_LC_921.cpp

---
+++ assignments/week2/day2/Create Saurav_Hossain_LC_921.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int minAddToMakeValid(string S) {
+        //we have a stack that holds chars ( & pops off when we see )
+        //we have a string NEED 
+            //if we see ( push onto stack mark ) into need
+            //if we next see that we get ) pop ( off stack and take away ) from need string 
+        
+        stack<char> holdOpenOnly;
+        string need = "";
+        
+        if(S[0] == ')')
+        {
+            need = need + '(';
+        }
+        
+        if(S[0] == '(')
+        {
+            holdOpenOnly.push(S[0]);
+            need = need + ')';
+        }
+        
+        for(int i = 1; i < S.length(); i++)
+        {
+            if(S[i] == '(')
+            {
+                holdOpenOnly.push(S[0]);
+                need = need + ')';
+            }
+            if(S[i] == ')')
+            {
+                if(!holdOpenOnly.empty())
+                {
+                    holdOpenOnly.pop();
+                    need = need.substr(0, need.length()-1);;
+                }
+                else
+                {
+                    need = need + '(';
+                }
+            }
+            
+        }
+        
+        //need should hold what is needed so return siz
+        return need.length();
+    }
+};
