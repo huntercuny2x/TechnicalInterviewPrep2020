fcca4b2489715f70a665a38d5ee88cdaf0cba414
talhar <delta.talha@gmail.com>
2020-06-09 23:29:26 -0400

    finished problem 55

--- assignments/week2/day2/Talha_Rahman_LC_55.cpp
+++ assignments/week2/day2/Talha_Rahman_LC_55.cpp
@@ -1,14 +1,19 @@
-#include <iostream>
 #include <vector>
 
+/**
+ * my approach is working backwords in the vector and seeing if I end up at the start.
+ * I have an end position, which is the last element, create a regular position which is 
+ * one less than that end position and then see if I can reach the end position with the 
+ * number of jumps available (nums[pos]). If I can reach the end_pos then I move the end_pos
+ * to the current position and then keep working down.
+ */
 bool canJump(std::vector<int>& nums) {
-    for(int i = 1; i < nums[0]; ++i) {
-        
-    }
-}
+    int end_pos = nums.size() - 1;
 
-int main() {
-    std::vector<int> nums{2, 3, 1, 1, 4};
-    
-    std::cout << canJump << "\n";
+    for(int pos = end_pos - 1; pos >= 0; --pos) {
+        if (pos + nums[pos] >= end_pos) 
+            end_pos = pos;
+    }
+
+    return end_pos == 0;
 }
