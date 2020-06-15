0efcf1b777d5c944431eed0af727cc4ea36937ce
hecris <christopher.he33@myhunter.cuny.edu>
2020-06-15 12:03:47 -0400

    add second DP solution to LC279

--- assignments/week3/day1/Christopher_He_LC_279.py
+++ assignments/week3/day1/Christopher_He_LC_279.py
@@ -1,28 +1,43 @@
 class Solution:
     def numSquares(self, n: int) -> int:
+	# solution with dynamic programming
+        def solution_dp(n):
+	    squares = []
+	    i = 1
+	    while i * i <= n:
+		squares.append(i * i)
+		i += 1
+
+	    dp = [0] * (n+1)
+
+	    for num in range(1, n+1):
+		mymin = n - 1
+		for sq in squares:
+		    if num - sq < 0:
+			break
+		    mymin = min(mymin, dp[num - sq])
+		dp[num] = mymin + 1
+
+	    return dp[n]
+
+        # solution with recursion + memoization
         def solution(n, memo):
-            if n in memo:
-                return memo[n]
+            if n == 1:
+                return 1
 
             if n < 1:
                 return 0
 
-            # print(n)
+            if n not in memo:
+                i = 2
+                mymin = n
+                while i * i <= n:
+                    mymin = min(mymin, solution(n - i * i, memo))
+                    i += 1
 
-            i = 1
-            mymin = n
-            while i * i <= n:
-                mymin = min(mymin, solution(n - i * i, memo))
-                i += 1
+                memo[n] = 1 + mymin
 
-            memo[n] = 1 + mymin
-            return 1 + mymin
+            return memo[n]
 
-
-        return solution(n, {})
-
-s = Solution()
-print(s.numSquares(10))
-print(s.numSquares(12))
-print(s.numSquares(13))
-print(s.numSquares(64))
+	return solution_dp(n)
+        # return solution(n, {})
