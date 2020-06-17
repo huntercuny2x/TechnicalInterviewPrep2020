2a4c731cef5fb5aaaa1258ce1da9b57962d2540f
hecris <christopher.he33@myhunter.cuny.edu>
2020-06-17 11:39:24 -0400

    240 add alternate (more optimal) solution

--- assignments/week3/day3/Christopher_He_LC_240.py
+++ assignments/week3/day3/Christopher_He_LC_240.py
@@ -1,4 +1,22 @@
 class Solution:
+    # more optimal solution
+    def searchMatrix2(self, matrix, target):
+	if not matrix:
+            return False
+
+        i, j = 0, len(matrix[0]) - 1
+
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                j -= 1
+
+        return False
+
+    # my original solution using binary search
     def searchMatrix(self, matrix, target):
         """
         :type matrix: List[List[int]]
